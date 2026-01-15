7d48649922d338fd76a85001b3fca9dca90085b1
Amir Freund <amir61@gmail.com>
2026-01-15 10:35:17 +0200

    Article 1 for review

--- daily_git.bat
+++ daily_git.bat
@@ -1,5 +1,10 @@
+@echo off
+set "msg=Daily update"
+set /p "user_msg=Enter commit message (Default: Daily update): "
+if not "%user_msg%"=="" set "msg=%user_msg%"
+
 git add .
-git commit -m "Daily update"
+git commit -m "%msg%"
 git push -u origin main
 git --no-pager log --oneline
 pause
